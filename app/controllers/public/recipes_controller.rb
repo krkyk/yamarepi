b46c397b4b72c8c1d8676b2836e66e3eede0e214
krkyk <madeinpink1111@gmail.com>
2022-05-07 13:40:55 +0000

    [Update]レシピ投稿機能/手順の追加

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -8,8 +8,9 @@ class Public::RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
+    @recipe.customer_id=current_customer.id
     if @recipe.save
-      redirect_to recipe_path(@recipe.id)
+      redirect_to recipe_path(@recipe.id) ,notice: "レシピを投稿しました"
     else
       render "new"
     end
@@ -29,4 +30,11 @@ class Public::RecipesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title,:serving,:description,steps_attributes:[:id,:step_description,:_destroy],recipe_ingredients_attributes:[:id,:quantity,:_destroy])
+  end
+
 end
