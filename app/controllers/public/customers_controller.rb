1704487036eddd372df1e34ff7b56d94a26aa741
krkyk <madeinpink1111@gmail.com>
2022-05-07 01:07:29 +0000

    [Add]ヘッダーとフッターの作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,19 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customer_path(current_customer.id) ,notice: "登録情報を更新しました"
+    else
+      render "edit"
+    end
   end
 
   def favorites
@@ -15,5 +23,17 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdraw
+    customer = current_customer
+    #is_deletedカラムをtrueに変更して削除フラグを立てる
+    customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path ,notice: "アカウントを削除しました"
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name,:email,:customer_image,:encrypted_password,:is_deleted)
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,10 +5,10 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :customer_image
-  
+
   def get_customer_image(width, height)
   unless customer_image.attached?
-    file_path = Rails.root.join('app/assets/images/no.jpg')
+    file_path = Rails.root.join('app/assets/images/noimage.jpg')
     customer_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
   customer_image.variant(resize_to_limit: [width, height]).processed
