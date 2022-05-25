d9874dc629b854a0749d85dc3ad4ab1313eecdb3
krkyk <madeinpink1111@gmail.com>
2022-05-25 09:04:10 +0000

    [Update]viewデザイン

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -10,15 +10,15 @@ $(document).on('turbolinks:load',function() {
 
 
 /* レシピ画像プレビュー */
-$(document).on('turbolinks:load',function (){
-  $('#recipe_recipe_image').on('change', function (e) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        $("#preview").attr('src', e.target.result);
-    }
-    reader.readAsDataURL(e.target.files[0]);
-});
-});
+// $(document).on('turbolinks:load',function (){
+//   $('#recipe_recipe_image').on('change', function (e) {
+//     var reader = new FileReader();
+//     reader.onload = function (e) {
+//         $("#preview").attr('src', e.target.result);
+//     }
+//     reader.readAsDataURL(e.target.files[0]);
+// });
+// });
 
 /* アバウトページの画像フェードイン */
 $(document).on('turbolinks:load',function () {
@@ -31,4 +31,46 @@ $(document).on('turbolinks:load',function () {
 /* フラッシュメッセージの消去 */
 $(document).on('turbolinks:load',function(){
   $('.flash').fadeOut(5000);
+});
+
+/*画像編集プレビュー*/
+// $(document).on('turbolinks:load',function (){
+// $("#recipe_recipe_img").on("change", function (e) {
+//     var files = e.target.files;
+//     var d = new $.Deferred().resolve();
+//     $.each(files, function (i, file) {
+//       d = d.then(function () {
+//         return previewImage(file);
+//       });
+//     });
+//   });
+
+//   var previewImage = function (imageFile) {
+//     var reader = new FileReader();
+//     var img = new Image();
+//     var def = $.Deferred();
+//     reader.onload = function (e) {
+//       // 画像を表示
+//       $("#image_preview").empty();
+//       $("#image_preview").append(img);
+//       img.src = e.target.result;
+//       def.resolve(img);
+//     };
+//     reader.readAsDataURL(imageFile);
+//     return def.promise();
+//   };
+// });
+
+$(function () {
+  $('#recipe_recipe_img').change(function (e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    reader.onload = (function (file) {
+      return function (e) {
+        $("#preview").attr("src", e.target.result);
+        $("#preview").attr("title", file.name);
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
 });
