cfb62dfa49e9d7d724c196564258ecd851d41c7e
krkyk <madeinpink1111@gmail.com>
2022-05-25 03:32:11 +0000

    [Update]ボタンデザイン

---
+++ db/seeds/production.rb
@@ -0,0 +1,14 @@
+Admin.create!(
+  email: 'admin@admin.com',
+  password: 'yamarepi'
+  )
+
+Tag.create!([
+  {name: "簡単"},
+  {name: "初心者におすすめ"},
+  {name: "クッカー"},
+  {name: "メスティン"},
+  {name: "ホットサンドメーカー"},
+  {name: "携帯食"},
+  {name: "キャンプ"},
+  ])
