ee8f4d08924fd8169870e4eb9282986683d22d54
krkyk <madeinpink1111@gmail.com>
2022-06-06 06:40:58 +0000

    [Add]掲示板コメント非同期

--- app/controllers/public/forum_comments_controller.rb
+++ app/controllers/public/forum_comments_controller.rb
@@ -1,7 +1,21 @@
 class Public::ForumCommentsController < ApplicationController
   def create
+    @forum = Forum.find(params[:forum_id])
+    @forum_comment = current_customer.forum_comments.new(forum_comment_params)
+    @forum_comment.forum_id = @forum.id
+    @forum_comment.save
+    @forum_comments = @forum.forum_comments.order(created_at: 'DESC').page(params[:page]).per(5)
   end
 
   def destroy
+    @forum = Forum.find(params[:forum_id])
+    ForumComment.find(params[:id]).destroy
+    @forum_comments = @forum.forum_comments.order(created_at: 'DESC').page(params[:page]).per(5)
+  end
+
+  private
+
+  def forum_comment_params
+    params.require(:forum_comment).permit(:forum_comment)
   end
 end
--- app/controllers/public/forum_reports_controller.rb
+++ app/controllers/public/forum_reports_controller.rb
@@ -1,12 +1,12 @@
 class Public::ForumReportsController < ApplicationController
   def create
-    @forum_report = Forum.find(params[:forum_id])
+    @forum = Forum.find(params[:forum_id])
     forum_report = current_customer.forum_reports.new(forum_id: @forum.id)
     forum_report.save
   end
 
   def destroy
-     @forum_report = Forum.find(params[:forum_id])
+    @forum = Forum.find(params[:forum_id])
     forum_report = current_customer.forum_reports.find_by(forum_id: @forum.id)
     forum_report.destroy
   end
